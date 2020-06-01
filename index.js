3857d4ece80455c6279f8a838f7a99698942d2aa
idetakumi <boc.10194@gmail.com>
2020-06-01 23:55:17 +0900

    dataTableの導入

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+'use strict'
 import Koa from 'koa'
 import views from 'koa-views'
 import router from './router'
+import serve from 'koa-static'
 const app = new Koa()
 app.use(views(__dirname + '/views/', { extension: 'pug' }))
 app.use(router.routes())
+app.use(serve(__dirname + '/public'))
 
 app.listen(process.env.PORT || 3000)
 console.log('Start.')
